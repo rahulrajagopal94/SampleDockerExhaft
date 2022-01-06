6bbd3e02932e620baeb185949b5bcbea160e0f95
Rahul <raahulonline@gmail.com>
2022-01-07 02:35:45 +0530

    Initial push

--- Configuration/Setup.cs
+++ Configuration/Setup.cs
@@ -53,8 +53,8 @@ namespace SampleDocker.Configuration
         [SetUp]
         public static void BeforeEachTest()
         {
-            driver = ConfigFile.Init("Configuration\\AppSetting.json");
-            Constant.SetConfig("Configuration\\AppSetting.json");
+            driver = ConfigFile.Init("Configuration/AppSetting.json");
+            Constant.SetConfig("Configuration/AppSetting.json");
         }
 
         [TearDown]
