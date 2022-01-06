161b6b67163ee6c2330c513d288e98fce4ac3a92
Rahul <raahulonline@gmail.com>
2022-01-07 04:52:26 +0530

    Initial

---
+++ DriverFile/DriverScript.cs
@@ -0,0 +1,47 @@
+﻿using Ex_haft.Configuration;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+
+
+namespace SampleDocker.DriverFile
+{
+    [TestFixture]
+    
+    
+    class DriverScript
+    {
+
+        /// <summary>
+        /// Driver Script-The Test Execution Starts from here
+        /// </summary>
+        //  [Test]
+        public void Test()
+        {
+            Assembly testAssembly;
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\SampleDocker.dll";
+            Console.WriteLine(assemblyFolder);
+            testAssembly = Assembly.LoadFile(assemblyFolder);
+            Console.WriteLine(testAssembly);
+            string json = ConfigFile.RetrieveTestScripts("Driver.json");
+            var scriptConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            foreach (var keyValue in scriptConfig)
+            {
+                if (keyValue.Value.Equals("true"))
+                {
+                    string className = "SampleDocker.TestScript." + keyValue.Key;
+                    Type runObject = testAssembly.GetType(className);
+                    Activator.CreateInstance(runObject);
+                }
+            }
+
+
+
+        }
+    }
+}
