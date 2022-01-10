560f0d9b4e44305fa8478811a47b1acbf72cc8bd
Rahul <raahulonline@gmail.com>
2022-01-10 23:47:23 +0530

    Updated Exhaft

---
+++ TestScript/LoginTest10.cs
@@ -0,0 +1,53 @@
+﻿using Ex_haft.Configuration;
+using NUnit.Framework;
+using SampleDocker.Configuration;
+using SampleDocker.Pages;
+
+
+namespace SampleDocker.TestScript
+{
+    class LoginTest10 : Setup
+    {
+        
+        public LoginTest10() : base()
+        {
+            testObjective = "To Verify that user is able to login to the web application.";
+            scriptName = "10Login to Application";
+            testData = ConfigFile.RetrieveInputTestData("LoginTest.json");
+
+            if (ConfigFile.IsRunFromDriverFile())
+                VerifyLogin10();
+        }
+
+
+       [Test, Category("Smoke")]
+        public void VerifyLogin10()
+        {
+            if (testData != null)
+            {
+                foreach (var input in testData)
+                {
+                    reporter = extent.CreateTest("LoginTest").Info("Login test started");
+
+                    report = LoginPage.LoginToApplication(driver, input, ref reporter);
+                    foreach (string screenshot in LoginPage.GetLoginPageScreenshots())
+                        screenshotList.Add(screenshot);
+
+                    report.AddRange(AddressPage.OpenAddressBook(driver, ref reporter));
+                    foreach (string screenshot in AddressPage.GetHomePageScreenshots())
+                        screenshotList.Add(screenshot);
+                 
+                    reporter.Info("Login test finished");
+                }
+            }
+
+            //Exit for driver file
+            if (ConfigFile.IsRunFromDriverFile())
+            {
+                Exit();
+                GenerateExtentReport();
+            }
+        }
+
+    }
+}
---
+++ TestScript/LoginTest15.cs
@@ -0,0 +1,53 @@
+﻿using Ex_haft.Configuration;
+using NUnit.Framework;
+using SampleDocker.Configuration;
+using SampleDocker.Pages;
+
+
+namespace SampleDocker.TestScript
+{
+    class LoginTest15 : Setup
+    {
+        
+        public LoginTest15() : base()
+        {
+            testObjective = "To Verify that user is able to login to the web application.";
+            scriptName = "15Login to Application";
+            testData = ConfigFile.RetrieveInputTestData("LoginTest5.json");
+
+            if (ConfigFile.IsRunFromDriverFile())
+                VerifyLogin15();
+        }
+
+
+       [Test, Category("Smoke")]
+        public void VerifyLogin15()
+        {
+            if (testData != null)
+            {
+                foreach (var input in testData)
+                {
+                    reporter = extent.CreateTest("LoginTest").Info("Login test started");
+
+                    report = LoginPage.LoginToApplication(driver, input, ref reporter);
+                    foreach (string screenshot in LoginPage.GetLoginPageScreenshots())
+                        screenshotList.Add(screenshot);
+
+                    report.AddRange(AddressPage.OpenAddressBook(driver, ref reporter));
+                    foreach (string screenshot in AddressPage.GetHomePageScreenshots())
+                        screenshotList.Add(screenshot);
+                 
+                    reporter.Info("Login test finished");
+                }
+            }
+
+            //Exit for driver file
+            if (ConfigFile.IsRunFromDriverFile())
+            {
+                Exit();
+                GenerateExtentReport();
+            }
+        }
+
+    }
+}
